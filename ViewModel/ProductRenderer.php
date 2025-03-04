c0df55e30a69c29fd267f508ffabef77e7197c66
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2025-03-04 12:17:05 +0100

    Added Model\ProductDisplayRequest and ViewModel\ProductRenderer, then added the ViewModel as an argument in view/frontend/layout/fecontroller_id_welcome_index.xml and finally called the ViewModel in view/frontend/templates/index.phtml

---
+++ ViewModel/ProductRenderer.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Denal05\Ad0e702ExerciseFrontendControllerMVVM\ViewModel;
+
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\NotFoundException;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Denal05\Ad0e702ExerciseFrontendControllerMVVM\Model\ProductDisplayRequest;
+class ProductRenderer implements ArgumentInterface
+{
+    /** @var ProductDisplayRequest */
+    private $displayRequest;
+
+    public function __construct(ProductDisplayRequest $displayRequest)
+    {
+        $this->displayRequest = $displayRequest;
+    }
+    public function getProduct(): ?ProductInterface
+    {
+        try {
+            $this->displayRequest->getProduct();
+        } catch (NotFoundException $ex) {
+            return null; // invalid product ID
+        } catch (NoSuchEntityException $ex) {
+            return null; // no ID specified in request
+        }
+    }
+}
