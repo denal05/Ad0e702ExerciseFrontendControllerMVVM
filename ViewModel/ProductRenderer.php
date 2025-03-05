60c8d537408f55d8c17967b0ea757ec6e12581fc
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2025-03-05 16:57:39 +0100

    Removed redundant Model\ProductDisplayRequest

--- ViewModel/ProductRenderer.php
+++ ViewModel/ProductRenderer.php
@@ -4,23 +4,39 @@ declare(strict_types=1);
 namespace Denal05\Ad0e702ExerciseFrontendControllerMVVM\ViewModel;
 
 use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\NotFoundException;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
-use Denal05\Ad0e702ExerciseFrontendControllerMVVM\Model\ProductDisplayRequest;
+
 class ProductRenderer implements ArgumentInterface
 {
-    /** @var ProductDisplayRequest */
-    private $displayRequest;
+    /** @var RequestInterface */
+    private $request;
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+    /** @var ProductInterface|null */
+    private $product;
 
-    public function __construct(ProductDisplayRequest $displayRequest)
-    {
-        $this->displayRequest = $displayRequest;
+    public function __construct(
+        RequestInterface           $request,
+        ProductRepositoryInterface $productRepository
+    ) {
+        $this->request = $request;
+        $this->productRepository = $productRepository;
     }
     public function getProduct(): ?ProductInterface
     {
         try {
-            return $this->displayRequest->getProduct();
+            if (!$this->product) {
+                $id = (int)$this->request->getParam('id');
+                if (!$id) {
+                    throw new NotFoundException(__('No product was specified.'));
+                }
+                $this->product = $this->productRepository->getById($id);
+            }
+            return $this->product;
         } catch (NotFoundException $ex) {
             return null; // invalid product ID
         } catch (NoSuchEntityException $ex) {
